4c78986fa8bb689cdc2fabd73ac3498ad2627d3a
robertgutu <gutu.robert49@yahoo.com>
2025-08-27 12:14:47 +0300

    reset inputs after submit

--- src/app/components/user-input/user-input.component.ts
+++ src/app/components/user-input/user-input.component.ts
@@ -10,10 +10,15 @@ import { InvestmentInput } from '../../models/investment.model';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  initialInvestment = signal<number>(0);
-  annualInvestment = signal<number>(0);
-  expectedReturn = signal<number>(5);
-  investmentDuration = signal<number>(10); 
+  initialInvestmentValue:number = 0;
+  annualInvestmentValue:number = 0;
+  expectedReturnValue:number = 5;
+  investmentDurationValue:number = 10;
+
+  initialInvestment = signal<number>(this.initialInvestmentValue);
+  annualInvestment = signal<number>(this.annualInvestmentValue);
+  expectedReturn = signal<number>(this.expectedReturnValue);
+  investmentDuration = signal<number>(this.investmentDurationValue); 
 
   @Output() calculate = new EventEmitter<InvestmentInput>();
   // calculate = output<InvestmentInput>();
@@ -28,9 +33,18 @@ export class UserInputComponent {
     };
     console.log("Input Data:  ", inputData);
     this.calculate.emit(inputData);
+    this.resetInputs();
   }
 
   onChangeInput() {
     this.updateInput.emit();
+  }
+
+  resetInputs() {
+    this.initialInvestment.set(this.initialInvestmentValue);
+    this.annualInvestment.set(this.annualInvestmentValue);
+    this.expectedReturn.set(this.expectedReturnValue);  
+    this.investmentDuration.set(this.investmentDurationValue);
+    
   }
 }
