0dcaba7f12dd2acebc5d5e3ac6b0b6a732c110ab
robertgutu <gutu.robert49@yahoo.com>
2025-08-27 11:11:08 +0300

    Move calculator function inside a service and inject service in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { UserInputComponent } from "./components/user-input/user-input.component";
 import { InvestmentInput, InvestmentResultItem } from './models/investment.models';
 import { ResultsComponent } from './components/results/results.component';
+import { CalculateService } from './services/calculate.service';
 
 @Component({
   selector: 'app-root',
@@ -15,35 +16,13 @@ export class AppComponent {
   isResultsAvailable = false;
   calculatedResults = signal<InvestmentResultItem[]>([]);
 
-  calculateInvestmentResults(data: InvestmentInput) {
-    const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-
-    // this.annualData.set(annualData);
-    return annualData;
-  }
+  // constructor(private calculate: CalculateService){}
+  calculate = inject(CalculateService);
 
   onCalculate(data: InvestmentInput) {
     this.isResultsAvailable = false;
     console.log("Calculating... for: ", data);
-    const results = this.calculateInvestmentResults(data);
+    const results = this.calculate.calculateInvestmentResults(data);
     console.log("Results: ", results);
     this.isResultsAvailable = true;
     this.calculatedResults.set(results);
